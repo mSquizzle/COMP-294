77dd3d2bedb0965b62562bb507f9540031eadd8b
mematthews <mary.matthews@velir.com>
2018-03-25 13:39:42 -0400

    Draw out axis labels.
    I also tracked down Processing's default font, woohoo.

--- js/chart.js
+++ js/chart.js
@@ -1,19 +1,84 @@
 const svgns = "http://www.w3.org/2000/svg";
 
-function drawSeries(chartParams, data, id){
+function drawChart(chartParams, dataSet){
+	var parentHeight = chartParams.height;
+	var parentWidth = chartParams.width;
+	var chartParent = chartParams.chartParent;
+	var dataAreaWidth = .9 * parentWidth;
+	var axes =  document.createDocumentFragment();
+	var leftAxis = .05 * parentWidth;
+	var rightAxis = parentWidth - leftAxis;
+	
+	var bottom = parentHeight * .9 ;
+	var top = parentHeight - bottom;
+
+	var lineList = document.createDocumentFragment();
+	var line = document.createElementNS(svgns, "line");
+	line.setAttribute("x1", leftAxis);
+	line.setAttribute("y1", top);
+	line.setAttribute("x2", leftAxis);
+	line.setAttribute("y2", bottom);
+	line.setAttribute("class", "axis");
+	line.setAttribute("stroke", "black");
+	lineList.appendChild(line);
+	line = document.createElementNS(svgns, "line");
+	line.setAttribute("x1", rightAxis);
+	line.setAttribute("y1", top);
+	line.setAttribute("x2", rightAxis);
+	line.setAttribute("y2", bottom);
+	line.setAttribute("class", "axis");
+	line.setAttribute("stroke", "black");
+	lineList.appendChild(line);
+	line = document.createElementNS(svgns, "line");
+	line.setAttribute("x1", leftAxis);
+	line.setAttribute("y1", bottom);
+	line.setAttribute("x2", rightAxis);
+	line.setAttribute("y2", bottom);
+	line.setAttribute("class", "axis");
+	line.setAttribute("stroke", "black");
+	lineList.appendChild(line);
+	var start = bottom;
+	var labelStepSize = chartParams.labelStepSize ? chartParams.labelStepSize : 5;
+	var scale = getScaleFunction(0, 25, bottom, top, 1);	
+	for(var i = 0; i <= chartParams.maxVal; i+= labelStepSize){
+		start = scale(i);
+		line = document.createElementNS(svgns, "line");
+		line.setAttribute("x1", leftAxis);
+		line.setAttribute("y1", start);
+		line.setAttribute("x2", leftAxis-15);
+		line.setAttribute("y2", start);
+		line.setAttribute("class", "axis");
+		line.setAttribute("stroke", "black");
+		var text =  document.createElementNS(svgns, "text");
+		text.setAttribute("x", 0);
+		text.setAttribute("y", start);
+		text.setAttribute("class", "axis");
+		text.setAttribute("stroke", "black");
+		text.innerHTML = i+"%";		
+		lineList.appendChild(line);
+		lineList.appendChild(text);
+	}
+	chartParent.appendChild(lineList);
+		
+	dataSet.forEach(function(element, i){
+		drawSeries(chartParams, bottom, top, element, i);
+	});
+}
+
+function drawSeries(chartParams, bottom, top, data, id){
 	var dataPoints = Object.entries(data);
+	var max = chartParams.maxVal;
 	
 	var parentHeight = chartParams.height;
 	var parentWidth = chartParams.width;
 	var chartParent = chartParams.chartParent;
-	var max = chartParams.maxVal;
-	var placementStep = parentWidth / (dataPoints.length-1);
-	var startX = 0;
+	var placementStep = .9 * parentWidth / (dataPoints.length);
+	var startX = .05 * parentWidth;
 	var circleList = document.createDocumentFragment();
 	var	lineList = document.createDocumentFragment();
 	
-	var scale = getScaleFunction(0, max, 0, parentHeight, .9);
-	var startY = parentHeight - scale(dataPoints[0][1]);
+	var scale = getScaleFunction(0, max, bottom, top, 1);
+	var startY = scale(dataPoints[0][1]);
 	var circle = document.createElementNS(svgns, "circle");
 	var formatter = getFormatter(chartParams);
 	circle.setAttribute("cx", startX);
@@ -29,10 +94,10 @@ function drawSeries(chartParams, data, id){
 	circleList.append(circle);
 	
 	var limit = dataPoints.length;
-	for (var i = 1; i < limit; i++){
+	for (var i = 0; i < limit; i++){
 		var point = dataPoints[i];
 		var nextX = startX + placementStep;
-		var nextY = parentHeight - scale(point[1]);
+		var nextY = scale(point[1]);
 		circle = document.createElementNS(svgns, "circle");
 		circle.setAttribute("cx", startX);
 		circle.setAttribute("cy", startY);
